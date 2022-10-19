f2854dacd9697a0bf07e6acf923410c4191a2997
Daniel <daniel.oliveira@foursys.com.br>
2022-10-19 09:37:47 -0300

    refactored the Code, added night animation

--- swiftUI_2/ContentView.swift
+++ swiftUI_2/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  swiftUI_2
 //
-//  Created by Josicleison on 18/10/22.
+//  Created by Josicleison on 19/10/22.
 //
 
 import SwiftUI
@@ -11,65 +11,29 @@ struct ContentView: View {
     
     var body: some View {
         
-        ZStack {
+        NavigationView {
             
-            LinearGradient(gradient: Gradient(colors: [Assets.color, .blue]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
+            ZStack(alignment: .trailing) {
                 
-                Text("Afeganistao, Rio de Janeiro")
-                    .font(.system(size: 32,
-                                  weight: .medium,
-                                  design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                Color.blue.ignoresSafeArea()
                 
-                Create.stack(spacing: 8,
-                             image: "cloud.sun.fill", imageWidth: 180, imageHeight: 180,
-                             temperature: 76, temperatureSize: 70)
-                    .padding(.bottom, 40)
-                
-                HStack(spacing: 20) {
+                VStack {
                     
-                    Create.stack("TUE", image: "cloud.sun.fill", temperature: 74)
-                    Create.stack("WED", image: "sun.max.fill", temperature: 88)
-                    Create.stack("THU", image: "wind.snow", temperature: 55)
-                    Create.stack("FRI", image: "sunset.fill", temperature: 60)
-                    Create.stack("SAT", image: "snow", temperature: 25)
-                }
-                
-                Spacer()
-                
-                Button {
+                    Spacer()
                     
-                    print("Toma!")
-                    
-                }
-                label: {
-                    
-                    Text("Change Day Time")
-                        .frame(width: 200, height: 50)
-                        .background(.yellow)
-                        .font(.system(size: 20,
-                                      weight: .black,
-                                      design: .default))
-                        .cornerRadius(10)
-                        .foregroundColor(.blue)
-                }
-                
-                Spacer()
+                    NavigationLink(destination: WheaterView()) {
+                        
+                        HStack {
+                            
+                            Text("Go")
+                                .foregroundColor(.white)
+                                .font(.system(size: 25, weight: .bold))
+                            
+                            Create.image("chevron.right", height: 20)
+                        }.padding(.trailing, 20)
+                    }
+                }.preferredColorScheme(.dark)
             }
-        }.preferredColorScheme(.light)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        
-        ContentView()
+        }.accentColor(.white)
     }
 }
