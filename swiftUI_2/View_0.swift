361ee360349728f662597f6f8378e7e014d94eb0
Daniel <daniel.oliveira@foursys.com.br>
2022-10-20 10:24:01 -0300

    Refactored the code

---
+++ swiftUI_2/View_0.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  swiftUI_2
+//
+//  Created by Josicleison on 19/10/22.
+//
+
+import SwiftUI
+
+struct View_0: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            ZStack(alignment: .trailing) {
+                
+                LinearGradient(gradient: Gradient(colors: [Assets.Colors.blue, .blue]),
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    
+                    Spacer()
+                    
+                    navigate(to: View_1())
+                    navigate(to: View_2())
+                    navigate(to: View_3())
+                        .navigationTitle("Main Menu")
+                }
+            }
+        }.accentColor(Assets.Colors.reverseDark)
+    }
+    
+    private func navigate(to view: some View) -> some View {
+        
+        let title = "\(type(of: view))"
+        
+        return NavigationLink(destination: view) {
+            
+            HStack {
+                
+                Create.text(title, textColor: Assets.Colors.reverseDark,
+                            size: 25, weight: .bold)
+                
+                Create.systemImage(Assets.Images.back,
+                                   imageColor: Assets.Colors.reverseDark,
+                                   height: 20)
+            }.padding(.trailing, 20)
+        }
+    }
+}
--- swiftUI_2/View_1.swift
+++ swiftUI_2/View_1.swift
@@ -1,8 +1,8 @@
 //
-//  ContentView.swift
+//  View_5.swift
 //  swiftUI_2
 //
-//  Created by Josicleison on 19/10/22.
+//  Created by Josicleison on 20/10/22.
 //
 
 import SwiftUI
@@ -11,39 +11,6 @@ struct View_1: View {
     
     var body: some View {
         
-        NavigationView {
-            
-            ZStack(alignment: .trailing) {
-                
-                LinearGradient(gradient: Gradient(colors: [Assets.Colors.blue, .blue]),
-                               startPoint: .topLeading,
-                               endPoint: .bottomTrailing)
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    
-                    Spacer()
-                    
-                    navigate(to: View_3(), "Go View_3")
-                    navigate(to: View_2(), "Go View_2")
-                }
-            }
-        }.accentColor(Assets.Colors.reverseDark)
-    }
-    
-    private func navigate(to view: some View,_ title: String) -> some View {
-        
-        NavigationLink(destination: view) {
-            
-            HStack {
-                
-                Create.text(title, textColor: Assets.Colors.reverseDark,
-                            size: 25, weight: .bold)
-                
-                Create.systemImage(Assets.Images.back,
-                                   imageColor: Assets.Colors.reverseDark,
-                                   height: 20)
-            }.padding(.trailing, 20)
-        }.navigationBarTitle("View_1")
+        Create.text("Hello Crazy World")
     }
 }
